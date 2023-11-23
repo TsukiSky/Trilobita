976ceaaf8a67b83025f5f1570e615ed8481db557
wangyanbao666 <yanbao_wang@mymail.sutd.edu.sg>
2023-11-23 23:35:56 +0800

    fix: message consumer

--- core/src/main/java/com/trilobita/core/messaging/MessageConsumer.java
+++ core/src/main/java/com/trilobita/core/messaging/MessageConsumer.java
@@ -21,7 +21,7 @@ import java.util.concurrent.ExecutionException;
  */
 @Slf4j
 public class MessageConsumer {
-    private static final boolean willLog = true;
+    private static final boolean willLog = false;
     private volatile boolean runFlag = false;
     private String topic;
     private final MessageAdmin messageAdmin = MessageAdmin.getInstance();
@@ -57,9 +57,9 @@ public class MessageConsumer {
         this.topic = topic;
     }
 
-    public MessageConsumer(String topic, String gid, MessageHandler messageHandler) {
+    public MessageConsumer(String topic, String serverId, MessageHandler messageHandler) {
         consumerProperties.putAll(messageAdmin.props);
-        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, gid); // Master topic probably is subscribed by multiple workers.
+        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "group-kafka-trilobita-"+ serverId+"-"+UUID.randomUUID()); // Master topic probably is subscribed by multiple workers.
         consumerProperties.put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, ("consumer-kafka-trilobita-" + topic)); // one worker has multiple consumer (group instance) differentiated by topic.
         consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         this.messageHandler = messageHandler;
@@ -68,7 +68,7 @@ public class MessageConsumer {
 
     public MessageConsumer(String topic, Integer serverId, MessageHandler messageHandler, String offsetPolicy) {
         consumerProperties.putAll(messageAdmin.props);
-        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "group-kafka-trilobita-"+ serverId); // Master topic probably is subscribed by multiple workers.
+        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "group-kafka-trilobita-"+ serverId+"-"+UUID.randomUUID()); // Master topic probably is subscribed by multiple workers.
         consumerProperties.put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, ("consumer-kafka-trilobita-" + topic)); // one worker has multiple consumer (group instance) differentiated by topic.
         consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offsetPolicy);
         this.messageHandler = messageHandler;
