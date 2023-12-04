93d239511c1769f7ec1fd9c44111ed2ec0f9b03b
Siqi <siqxiang@gmail.com>
2023-12-04 19:48:37 +0800

    resolve minor errors

--- runtime/src/main/java/com/trilobita/runtime/parser/JsonParser.java
+++ runtime/src/main/java/com/trilobita/runtime/parser/JsonParser.java
@@ -1,7 +1,7 @@
 package com.trilobita.runtime.parser;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.trilobita.commons.Computable;
+import com.trilobita.core.common.Computable;
 import com.trilobita.core.graph.Graph;
 import com.trilobita.core.graph.vertex.Vertex;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +10,6 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
